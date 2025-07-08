4f9c0c006ab12f1a22431890f5f0129d6f5f49e2
Pramitha Dewi <dipramithadewi@gmail.com>
2025-07-08 21:36:31 +0700

    Membuat isi section NDBI di tab Pengolahan Time-Series dan menambahkan st.expander untuk daftar referensi.

--- howItsMade/workflow.py
+++ howItsMade/workflow.py
@@ -90,7 +90,7 @@ tab1, tab2, tab3, tab4 = st.tabs(
 
 with tab1:
     st.header("Prapengolahan Data")
-    # Filtering Citra
+    # Penyaringan Citra
     st.badge("**Penyaringan Citra**", color="primary")
     st.markdown(
         """
@@ -101,23 +101,23 @@ with tab1:
         unsafe_allow_html=True,
     )
 
-    codeScalingFactor = """
-var landsat = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
+    codePenyaringanCitra = """
+var landsat2024 = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
     .filterDate('2024-04-21', '2024-10-31') // Mengacu pada kajian Periode Normal Rata-rata Klimatologi 1991-2020 dari Badan Meteorologi Klimatologi dan Geofisika
     .filterBounds(loc)
-    .map(applyScaleFactors) // Function Scaling Factor
+    .map(applyScaleFactors8) // Function Scaling Factor
     .map(maskLsr) // Function Cloud Masking
     .median() // Median Composite
     .clip(loc);
 """
-    st.code(codeScalingFactor, language="javascript", line_numbers=True)
+    st.code(codePenyaringanCitra, language="javascript", line_numbers=True)
 
     # Scaling Factor
     st.badge("**Scaling Factor**", color="primary")
     st.markdown(
         """
         <div class="justified-text">
-        <strong>Scaling Factor</strong> digunakan untuk mengembalikan nilai radiansi dan reflektansi citra Landsat Surface Reflectance yang sebelumnya berformat <strong>integer</strong> menjadi <strong>float</strong> agar hasil pengolahan data memiliki ketelitian hingga tingkat desimal (USGS, 2020).
+        <strong>Scaling Factor</strong> digunakan untuk mengembalikan nilai radiansi dan reflektansi citra Landsat Surface Reflectance yang sebelumnya berformat <strong>integer</strong> menjadi <strong>float</strong> agar hasil pengolahan data memiliki ketelitian hingga tingkat desimal (USGS, 2023).
         </div>
         """,
         unsafe_allow_html=True,
@@ -144,7 +144,7 @@ function applyScaleFactors(image) {
         unsafe_allow_html=True,
     )
 
-    codeScalingFactor = """
+    codeCloudMasking = """
 function maskLsr(image) {
   var cloudShadowBitMask = (1 << 4);
   var cloudsBitMask = (1 << 3);
@@ -156,11 +156,11 @@ function maskLsr(image) {
   return image.updateMask(mask);
 }
 """
-    st.code(codeScalingFactor, language="javascript", line_numbers=True)
+    st.code(codeCloudMasking, language="javascript", line_numbers=True)
 
     # Hasil Prapengolahan Data
     st.write(
-        "Berikut ini merupakan contoh tampilan citra Landsat 8 Surface Reflectance tahun 2024 sebelum dan sesudah dilakukan tahap pembersihan awan:"
+        "Berikut ini merupakan contoh tampilan citra **Landsat 8 Surface Reflectance** tahun 2024 **sebelum** dan **sesudah** dilakukan tahap pembersihan awan:"
     )
     from streamlit_image_comparison import image_comparison
 
@@ -176,6 +176,14 @@ function maskLsr(image) {
     )
     st.markdown("<div style='margin-bottom: 0.5rem;'></div>", unsafe_allow_html=True)
 
+    with st.expander("Lihat Referensi"):
+        st.markdown(
+            """
+                - Sinabutar, J. J., Sasmito, B., Sukmono, A. (2020). Studi Cloud Masking Menggunakan Band Quality Assessment, Function of Mask, dan Multi-Temporal Cloud Masking pada Citra Landsat 8. *Jurnal Geodesi Undip*, 9(3), 51-60. https://doi.org/10.14710/jgundip.2020.28123 
+                - United States Geological Survey. (2023). How Do I Use A Scale Factor with Landsat Level-2 Science Products?. (*https://www.usgs.gov/faqs/how-do-i-use-a-scale-factor-landsat-level-2-science-products*, diakses 8 Juli 2025).
+                """
+        )
+
 # tab 2
 with tab2:
     st.header("Pengolahan Time-Series")
@@ -190,7 +198,7 @@ with tab2:
         st.markdown(
             """
             <div class="justified-text">
-            Ekstraksi Land Surface Temperature (LST) dalam penelitian ini menggunakan metode <strong>Single-Channel</strong> yang dikembangkan oleh Jiménez-Muñoz & Sobrino (2009). Metode ini terdiri atas empat tahap perhitungan, yaitu:
+            Ekstraksi Land Surface Temperature (LST) dalam penelitian ini menggunakan metode <strong>Single-Channel</strong> yang dikembangkan oleh Jiménez-Muñoz & Sobrino (2010). Metode ini terdiri atas empat tahap perhitungan, yaitu:
         </div>
         """,
             unsafe_allow_html=True,
@@ -215,7 +223,7 @@ with tab2:
         st.markdown(
             """
         <div class="justified-text">
-        Emisivitas permukaan (ε) adalah kemampuan suatu objek dalam menyerap radiasi matahari dan memancarkan radiasi termal (Mallick et al., 2012). Penelitian ini menggunakan pendekatan NDVI dan Proportion of Vegetation (Pv) untuk mendapatkan nilai emisivitas permukaan. Berikut ini contoh rumus dan implementasi kode dalam Google Earth Engine.
+        Emisivitas permukaan (ε) adalah kemampuan suatu objek dalam menyerap radiasi matahari dan memancarkan radiasi termal (Mallick et al., 2012). Penelitian ini menggunakan pendekatan NDVI dan Proportion of Vegetation (Pv) untuk mendapatkan nilai emisivitas permukaan. Berikut ini rumus dan contoh implementasi kode dalam Google Earth Engine.
         </div>
         """,
             unsafe_allow_html=True,
@@ -247,11 +255,13 @@ with tab2:
         )
 
         # Kode NDVI
-        codeNDVIEmisi = """
+        codeNDVI = """
 // Perhitungan NDVI
-var ndvi = landsat.normalizedDifference(['SR_B5', 'SR_B4']).rename('ndvi');
+var ndvi1999 = landsat1999.normalizedDifference(['SR_B4', 'SR_B3']).rename('ndvi1999'); // Landsat 5
+var ndvi2004 = landsat2004.normalizedDifference(['SR_B4', 'SR_B3']).rename('ndvi2004'); // Landsat 7
+var ndvi2024 = landsat2024.normalizedDifference(['SR_B5', 'SR_B4']).rename('ndvi2024'); // Landsat 8
 """
-        st.code(codeNDVIEmisi, language="javascript", line_numbers=True)
+        st.code(codeNDVI, language="javascript", line_numbers=True)
 
         # Rumus Proporsi Vegetasi
         def display_equation(title, equation):
@@ -282,21 +292,21 @@ var ndvi = landsat.normalizedDifference(['SR_B5', 'SR_B4']).rename('ndvi');
         # Kode Pv
         codePv = """
 // Perhitungan Proportion of Vegetation (Pv)
-var ndviMin = ee.Number(ndvi.reduceRegion({
+var ndvi2024min = ee.Number(ndvi2024.reduceRegion({
   reducer: ee.Reducer.min(),
   geometry: loc,
   scale: 30,
   maxPixels: 1e9
 }).values().get(0));
 
-var ndviMax = ee.Number(ndvi.reduceRegion({
+var ndvi2024max = ee.Number(ndvi2024.reduceRegion({
   reducer: ee.Reducer.max(),
   geometry: loc,
   scale: 30,
   maxPixels: 1e9
 }).values().get(0));
 
-var pv = (ndvi.subtract(ndviMin).divide(ndviMax.subtract(ndviMin))).pow(ee.Number(2)).rename('pv');
+var pv2024 = (ndvi2024.subtract(ndvi2024min).divide(ndvi2024max.subtract(ndvi2024min))).pow(ee.Number(2)).rename('pv2024');
 """
         st.code(codePv, language="javascript", line_numbers=True)
 
@@ -328,7 +338,7 @@ var pv = (ndvi.subtract(ndviMin).divide(ndviMax.subtract(ndviMin))).pow(ee.Numbe
 // Perhitungan Emisivitas Permukaan (ε)
 var k1 = ee.Number(0.004);
 var k2 = ee.Number(0.986);
-var emisivitas = pv.multiply(k1).add(k2).rename('emisivitas');
+var emisi2024 = pv2024.multiply(k1).add(k2).rename('emisi2024');
 """
         st.code(codeEmisivitasPermukaan, language="javascript", line_numbers=True)
 
@@ -337,7 +347,7 @@ var emisivitas = pv.multiply(k1).add(k2).rename('emisivitas');
         st.markdown(
             """
         <div class="justified-text">
-        Brightness temperature merupakan representasi suhu permukaan yang diperoleh dari radiasi termal objek kemudian direkam oleh sensor termal dan disajikan dalam satuan kelvin (Jatayu & Susetyo, 2017). Penerapan function Scaling Factor di tahapan sebelumnya telah mengkalibrasi saluran termal ke dalam satuan kelvin sehingga dapat langsung dimanfaatkan sebagai nilai brightness temperature (Waleed & Sajjad, 2021). Adapun saluran termal yang digunakan berasal dari band 6 untuk citra Landsat 5 dan Landsat 7, serta band 10 untuk citra Landsat 8.
+        Brightness temperature merupakan representasi suhu permukaan yang diperoleh dari radiasi termal objek kemudian direkam oleh sensor termal dan disajikan dalam satuan kelvin (Jatayu & Susetyo, 2017). Penerapan function Scaling Factor di tahapan sebelumnya telah mengkalibrasi saluran termal ke dalam satuan kelvin sehingga dapat langsung dimanfaatkan sebagai nilai brightness temperature (Waleed & Sajjad, 2022). Adapun saluran termal yang digunakan berasal dari band 6 untuk citra Landsat 5 dan Landsat 7, serta band 10 untuk citra Landsat 8.
         </div>
         """,
             unsafe_allow_html=True,
@@ -345,9 +355,9 @@ var emisivitas = pv.multiply(k1).add(k2).rename('emisivitas');
 
         codeBrightnessTemperature = """
 // Pemilihan Saluran Termal untuk Brightness Temperature
-var btLandsat5 = landsat5.select('ST_B6');
-var btLandsat7 = landsat7.select('ST_B6');
-var btLandsat8 = landsat8.select('ST_B10');
+var bt1999 = landsat1999.select('ST_B6');
+var bt2004 = landsat2004.select('ST_B6');
+var bt2024 = landsat2024.select('ST_B10');
 """
         st.code(codeBrightnessTemperature, language="javascript", line_numbers=True)
 
@@ -355,7 +365,7 @@ var btLandsat8 = landsat8.select('ST_B10');
         st.markdown(
             """
         <div class="justified-text">
-        Perhitungan LST melibatkan nilai brightness temperature, emisivitas permukaan, panjang gelombang saluran termal, serta nilai radiasi emisivitas yang diestimasi dari konstanta Planck, Stefan-Boltzmann, dan kecepatan cahaya (Waleed & Sajjad, 2021). Berikut ini contoh rumus dan implementasi kode dalam Google Earth Engine.
+        Perhitungan LST melibatkan nilai brightness temperature, emisivitas permukaan, panjang gelombang saluran termal, serta nilai radiasi emisivitas yang diestimasi dari konstanta Planck, Stefan-Boltzmann, dan kecepatan cahaya (Waleed & Sajjad, 2022). Berikut ini rumus dan contoh implementasi kode dalam Google Earth Engine.
         </div>
         """,
             unsafe_allow_html=True,
@@ -370,7 +380,7 @@ var btLandsat8 = landsat8.select('ST_B10');
             # )
 
         display_equation(
-            "Formula LST",
+            "Rumus LST",
             r"LST = \left( \frac{B_T}{1 + \left( \frac{\lambda \cdot B_T}{\rho} \right) \cdot \ln \epsilon} \right) - 273.15",
         )
 
@@ -390,17 +400,76 @@ var btLandsat8 = landsat8.select('ST_B10');
 
         codeLST = """
 // Perhitungan LST
-var lst = btLandsat8.expression(
+var lst2024 = bt2024.expression(
   '(Bt/(1+(0.00115*(Bt/1.438))*log(Ep)))-273.15', {
-    'Bt': bt,
-    'Ep': emisi
-  }).rename('lst');
+    'Bt': bt2024,
+    'Ep': emisi2024
+  }).rename('lst2024');
 """
         st.code(codeLST, language="javascript", line_numbers=True)
 
+        with st.expander("Lihat Referensi"):
+            st.markdown(
+                """
+                - Jatayu, A., & Susetyo, C. (2017). Analisis Perubahan Temperatur Permukaan Wilayah Surabaya Timur Tahun 2001-2016 Menggunakan Citra Landsat. *Jurnal Teknik ITS*, 6(2). 429-433. 
+                - Jiménez-Muñoz, J. C., & Sobrino, J. A. (2010). A Single-Channel Algorithm for Land-Surface Temperature Retrieval from ASTER Data. *IEEE Geoscience and Remote Sensing Letters*, 7(1). 176-179. https://doi.org/10.1109/LGRS.2009.2029534
+                - Mallick, J., Singh, C. K., Shashtri, S. Rahman, A., Mukherjee, S. (2012). Land Surface Emissivity Retrieval based on Moisture Index from Landsat TM Satellite Data over Heterogeneous Surfaces of Delhi City. *Internasional Journal of Applied Earth Observation and Geoinformation*, 19, 384-358. https://doi.org/10.1016/j.jag.2012.06.002
+                - Waleed, M., & Sajjad, M. (2022). Leveraging Cloud-based Computing and Spatial Modelling Approaches for Land Surface Temperature Disparities in Response to Land Cover Change: Evidence from Pakistan. *Remote Sensing Applications: Society and Environment*, 25, 1-19. https://doi.org/10.1016/j.rsase.2021.100665 
+                """
+            )
+
     elif option == "NDBI":
-        st.badge("**Normalized Difference Built-Up Index (NDBI)**", color="primary")
-        st.write("NDBI adalah indeks kerapatan bangunan")
+        st.subheader("**Normalized Difference Built-Up Index (NDBI)**")
+        st.markdown(
+            """
+            <div class="justified-text">
+            Perhitungan NDBI diestimasi dengan memanfaatkan saluran reflektif inframerah-dekat (NIR) dan inframerah-gelombang pendek (SWIR) yang sangat sensitif terhadap area terbangun (Wicaksono et al., 2021). Berikut ini rumus dan contoh implementasi kode dalam Google Earth Engine.
+        </div>
+        """,
+            unsafe_allow_html=True,
+        )
+
+        # Rumus NDBI
+        def display_equation(title, equation):
+            st.markdown(f"**{title}**")
+            st.latex(equation)
+            # st.markdown(
+            #     "<div style='margin-bottom: 0.5rem;'></div>", unsafe_allow_html=True
+            # )
+
+        display_equation(
+            "Rumus NDBI",
+            r"NDVI = \frac{SWIR - NIR}{SWIR + NIR}",
+        )
+
+        st.markdown(
+            """
+        <div class="justified-text">
+        <strong>Keterangan:</strong><br>
+        NDBI = Normalized Difference Built-Up Index<br>
+        SWIR = Band 5 (Landsat 5 dan Landsat 7), Band 6 (Landsat 8)<br>
+        NIR = Band 4 (Landsat 5 dan Landsat 7), Band 5 (Landsat 8)<br>
+        </div>
+        """,
+            unsafe_allow_html=True,
+        )
+
+        # Kode NDBI
+        codeNDBI = """
+// Perhitungan NDBI
+var ndbiLandsat5 = landsat5.normalizedDifference(['SR_B5', 'SR_B4']).rename('ndbiLandsat5');
+var ndbiLandsat7 = landsat7.normalizedDifference(['SR_B5', 'SR_B4']).rename('ndbiLandsat7');
+var ndbiLandsat8 = landsat8.normalizedDifference(['SR_B6', 'SR_B5']).rename('ndbiLandsat8');
+"""
+        st.code(codeNDBI, language="javascript", line_numbers=True)
+
+        with st.expander("Lihat Referensi"):
+            st.markdown(
+                """
+                - Wicaksono, C. S. A., Sukmono, A., Hadi, F. (2021). Analisis Pengaruh Perubahan Komposisi Vegetasi dan Kawasan Terbangun terhadap Suhu Permukaan (Studi Kasus: Kota Tegal). *Jurnal Geodesi Undip*, 10(3), 1-10. https://doi.org/10.14710/jgundip.2021.31120 
+                """
+            )
+
     elif option == "NDMI":
         st.badge("**Normalized Difference Moisture Index (NDMI)**", color="primary")
         st.write("NDBI adalah indeks kelembapan vegetasi")
