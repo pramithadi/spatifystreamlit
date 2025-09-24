18f7133d8265ef6ff6b636722fa7cde5bedc1510
Pramitha Dewi <dipramithadewi@gmail.com>
2025-09-25 00:48:08 +0700

    Mencoba hide parameters except LST.

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ gee_apps = st.Page(
 pg = st.navigation(
     {
         "": [home],
-        "Dasbor Analisis": [lst, ndbi, ndmi, ndvi, lulc],
+        "Dasbor Analisis": [lst],
         "Telisik Proses": [alat, dataset, metodologi],
         "Eksplorasi Hasil": [download_peta, gee_apps],
     }
